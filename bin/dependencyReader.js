0c3b25a5422a9de22da882f99a2f96ea559cf924
Swati Kode <swatikode@gmail.com>
2019-09-19 13:50:30 -0500

    Called the releases v3 api to get release info

--- bin/dependencyReader.js
+++ bin/dependencyReader.js
@@ -1,7 +1,43 @@
 const process = require("process");
 const fs = require("fs");
-process.argv.slice(2).forEach(function (arg) {
+const axios = require("axios");
+
+// Example of how to read the process arguments
+// Will be needed to read the name of the consuming repo in future
+process.argv.slice(2).forEach(function(arg) {
     console.log(arg);
+});
+
+fs.readFile('./bin/dependencies.json', 'utf8', (err, data) => {
+    if (err) {
+        console.log("-------------- Error --------------");
+        console.log(err);
+    }
+    else {
+        console.log("------------- Listing the dependencies ------------");
+        const dependenciesList = JSON.parse(data).dependencies;
+        for (let dependency in dependenciesList) {
+            if (dependenciesList.hasOwnProperty(dependency)) {
+                // Do things here
+                console.log(dependency);
+            }
+        }
+
+        // Grab the release information of dependencies
+        // To begin with let's get info of carbon-graphs
+        axios
+            .get("https://api.github.com/repos/cerner/carbon-graphs/releases")
+            .then((response) => {
+                const releases = response.data;
+                const latestRelease = releases.find(
+                    release => !release.prerelease && !release.draft
+                );
+                console.log(latestRelease.tag_name);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
 });
 let a = "inside a javascript file";
 console.log(a);
